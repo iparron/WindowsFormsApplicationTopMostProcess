91ff902331ec905f3cc40e26c60d0b4ba11f6069
iker <iparron@dibal.com>
2018-02-27 11:18:31 +0100

    Agregar archivos de proyecto.

---
+++ WindowsFormsApplicationTopMostProcess/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApplicationTopMostProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(151, 56);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(151, 125);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApplicationTopMostProcess/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplicationTopMostProcess
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        public static int GWL_STYLE = -16;
+        public static int WS_CHILD = 0x40000000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }              
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("done");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Process.Start("calc.exe");
+            Process calc = Process.GetProcessesByName("calc")[0];
+            if (calc != null)
+            {
+                Hide();
+                FormBorderStyle = FormBorderStyle.None;
+                SetBounds(0, 0, 0, 0, BoundsSpecified.Location);
+
+                IntPtr hostHandle = calc.MainWindowHandle;
+                IntPtr guestHandle = this.Handle;
+                SetWindowLong(guestHandle, GWL_STYLE, GetWindowLong(guestHandle, GWL_STYLE) | WS_CHILD);
+                SetParent(guestHandle, hostHandle);
+
+                Show();
+            }
+        }
+    }
+}
